8ba19836ec2d3cf26023cc426dbde9ff8d6b57c3
Brayan Elian Jaimes <brepenaj@udistrital.edu.co>
2024-05-12 04:27:35 -0500

    feat: :alien: add DataLoader for Unit Service

--- main/src/main/java/com/main/utils/DataLoader.java
+++ main/src/main/java/com/main/utils/DataLoader.java
@@ -11,6 +11,7 @@ import java.util.function.Consumer;
 
 import com.main.dtos.AdminRequest;
 import com.main.dtos.EmployeeRequest;
+import com.main.dtos.ServiceUnitRequest;
 import com.main.dtos.StudentRequest;
 import com.main.services.UserService;
 
@@ -26,9 +27,14 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         if (isDatabaseEmpty()) {
-            loadData("classpath:data/administrators.json", new TypeReference<List<AdminRequest>>() {}, userService::registerAdministrator);
-            loadData("classpath:data/employees.json", new TypeReference<List<EmployeeRequest>>() {}, userService::registerEmployee);
-            loadData("classpath:data/students.json", new TypeReference<List<StudentRequest>>() {}, userService::registerStudent);
+            loadData("classpath:data/administrators.json", new TypeReference<List<AdminRequest>>() {
+            }, userService::registerAdministrator);
+            loadData("classpath:data/employees.json", new TypeReference<List<EmployeeRequest>>() {
+            }, userService::registerEmployee);
+            loadData("classpath:data/students.json", new TypeReference<List<StudentRequest>>() {
+            }, userService::registerStudent);
+            loadData("classpath:data/units.json", new TypeReference<List<ServiceUnitRequest>>() {
+            }, userService::registerServiceUnit);
         }
     }
 
